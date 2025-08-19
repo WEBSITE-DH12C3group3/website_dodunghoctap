e8b69f4eb16d3d01c40a73d28e041e1afa748cb8
ナム <n4mc0de@gmail.com>
2025-08-19 08:46:04 +0700

    Reapply "ngu"
    This reverts commit 16c3e03de5489897fb60aebb81aeddd560d65b90.

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const session = require("express-session");
 const favoriteRoutes = require("./routes/favorites.routes");
 const userRoutes = require("./routes/user.routes");
 const cartRoutes = require("./routes/cart.routes");
+const adminRoutes = require("./routes/product.routes");
+const authRoutes = require("./routes/auth.routes");
+const resetPasswordRoutes = require("./routes/resetpassword.routes");
+
 const db = require("./config/db");
-const productRoutes = require('./routes/product.routes');
+const productRoutes = require("./routes/product.routes");
 // Import middleware
 const setUser = require("./middlewares/setUser");
 
@@ -29,7 +33,7 @@ app.use(
 app.use(setUser);
 //kiểm tra người dùng đã đăng nhập hay chưa
 app.use((req, res, next) => {
-  res.locals.user = req.session.user || null; 
+  res.locals.user = req.session.user || null;
   next();
 });
 
@@ -41,14 +45,14 @@ app.use((req, res, next) => {
 
 // Trang chủ
 app.get("/", (req, res) => {
-  res.render("pages/home", { 
+  res.render("pages/home", {
     user: req.session.user || null,
-    title: "Trang chủ"
+    title: "Trang chủ",
   });
 });
 
 // Trang đăng nhập
-app.use("/", require("./routes/auth.routes"));
+app.use("/", authRoutes);
 // Trang thông tin tài khoản
 app.use("/", userRoutes);
 
@@ -64,10 +68,9 @@ app.use("/liked", favoriteRoutes);
 app.use("/cart", cartRoutes);
 
 // Admin routes
-app.use("/admin", require("./routes/product.routes"));
+app.use("/admin", adminRoutes);
 
 // Trang đặt lại mật khẩu
-const resetPasswordRoutes = require("./routes/resetpassword.routes");
 app.use("/resetpassword", resetPasswordRoutes);
 
 app.listen(PORT, () => {
